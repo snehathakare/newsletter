f401cc94c41ab19b7c9f750084e4714c60d55edf
Sneha <snhthakare@gmail.com>
2022-01-27 18:47:39 +0530

    Adds css file

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const app = express()
 let ejs = require('ejs');
 
 app.use(bodyParser.urlencoded({ extended: true }))
+app.use(express.static("public"))
 app.set('view engine', 'ejs');
 
 const users = [
